3074bd3767ed35bf71e8880a6810e16367317ed5
Nicola Manzini <nmanzini@cs.uchicago.edu>
2019-05-16 17:06:07 -0500

    reasonable

--- client.py
+++ client.py
@@ -3,16 +3,28 @@ import sys, json, requests
 IP = "http://127.0.0.1"
 
 def send_key_value(port,key,value):
-    server_address = IP + ':' + port + "/store"
+    server_address = IP + ':' + port + "/put"
     print (server_address)
-    dictionary = {
+    packet = {
         'key':key,
         'value':value
     }
-    packet = dictionary
     print(f"Sending: {packet}")
     response = requests.post(server_address, json=packet)
     print (response)
+
+def get(port,key):
+    server_address = IP + ':' + port + "/get"
+    print (server_address)
+    packet = {
+        'key':key,
+    }
+    print(f"Sending: {packet}")
+    response = requests.get(server_address, json=packet)
+    if response.status_code == 200:
+        print (response.json())
+    else:
+        print(f"{key} not found")
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
@@ -21,6 +33,8 @@ if __name__ == "__main__":
         key = sys.argv[2]
         value = sys.argv[3]
         send_key_value(ports[0],key,value)
+        key = "aaaaa"
+        get(ports[0],key)
 
     else:
         print("normal usage: python3 client.py <port0-port1-..> 'key' 'value'")
--- server.py
+++ server.py
@@ -1,25 +1,36 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import sys, json
 
 app = Flask(__name__)
 
 DATABASE = {}
 
-@app.route('/store', methods=['POST'])
-def success():
-    
+@app.route('/put', methods=['POST'])
+def put():
     packet = request.json
-    print(f"Received {packet}")
+    print(f"Received put request{packet}")
     key = packet["key"]
     value = packet["value"]
     DATABASE[key] = value 
 
-    return jsonify({"code" : 202})
+    return Response(status = 202)
+
+@app.route('/get', methods=['GET'])
+def get():
+    packet = request.json
+    print(f"Received get request {packet}")
+    key = packet["key"]
+    if key in DATABASE:
+        return jsonify(value = DATABASE[key])
+    else:
+        return Response(status = 404)
+
+
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         port = int (sys.argv[1])
-        app.run(host='127.0.0.1',port=port, debug=True)
+        app.run(host='127.0.0.1',port=port, debug=True,threaded = True)
     else:
         print("usage: python3 server.py <port>")
