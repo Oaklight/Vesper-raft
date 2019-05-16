a4315e47c5c98e0854f5c6af1e955815c4d82027
Nicola Manzini <nmanzini@cs.uchicago.edu>
2019-05-16 17:12:33 -0500

    formatted

--- client.py
+++ client.py
@@ -2,29 +2,29 @@ import sys, json, requests
 
 IP = "http://127.0.0.1"
 
-def send_key_value(port,key,value):
+
+def send_key_value(port, key, value):
     server_address = IP + ':' + port + "/put"
-    print (server_address)
-    packet = {
-        'key':key,
-        'value':value
-    }
+    print(server_address)
+    packet = {'key': key, 'value': value}
     print(f"Sending: {packet}")
     response = requests.post(server_address, json=packet)
-    print (response)
+    print(response)
 
-def get(port,key):
+
+def get(port, key):
     server_address = IP + ':' + port + "/get"
-    print (server_address)
+    print(server_address)
     packet = {
-        'key':key,
+        'key': key,
     }
     print(f"Sending: {packet}")
     response = requests.get(server_address, json=packet)
     if response.status_code == 200:
-        print (response.json())
+        print(response.json())
     else:
         print(f"{key} not found")
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
@@ -32,9 +32,9 @@ if __name__ == "__main__":
         ports = [port for port in sys.argv[1].split('-')]
         key = sys.argv[2]
         value = sys.argv[3]
-        send_key_value(ports[0],key,value)
+        send_key_value(ports[0], key, value)
         key = "aaaaa"
-        get(ports[0],key)
+        get(ports[0], key)
 
     else:
         print("normal usage: python3 client.py <port0-port1-..> 'key' 'value'")
--- server.py
+++ server.py
@@ -5,15 +5,16 @@ app = Flask(__name__)
 
 DATABASE = {}
 
+
 @app.route('/put', methods=['POST'])
 def put():
     packet = request.json
     print(f"Received put request{packet}")
     key = packet["key"]
     value = packet["value"]
-    DATABASE[key] = value 
+    DATABASE[key] = value
+    return Response(status=202)
 
-    return Response(status = 202)
 
 @app.route('/get', methods=['GET'])
 def get():
@@ -21,16 +22,14 @@ def get():
     print(f"Received get request {packet}")
     key = packet["key"]
     if key in DATABASE:
-        return jsonify(value = DATABASE[key])
+        return jsonify(value=DATABASE[key])
     else:
-        return Response(status = 404)
-
-
+        return Response(status=404)
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        port = int (sys.argv[1])
-        app.run(host='127.0.0.1',port=port, debug=True,threaded = True)
+        port = int(sys.argv[1])
+        app.run(host='127.0.0.1', port=port, debug=True, threaded=True)
     else:
         print("usage: python3 server.py <port>")
