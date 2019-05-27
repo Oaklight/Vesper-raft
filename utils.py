c138691546f942d6638ba06f0f966234690d1e67
Nicola Manzini <nmanzini@cs.uchicago.edu>
2019-05-26 21:09:25 -0500

    create_url

--- utils.py
+++ utils.py
@@ -6,5 +6,26 @@ def get_timeout():
     return random.randrange(cfg.LOW_TIMEOUT, cfg.HIGH_TIMEOUT)
 
 
+# def timeout_loop():
+#     loop = true
+#     while loop:
+
+# /follower
+# /leader
+
+
+def create_url(route, attributes=None):
+    # given attributes it create a url route
+    # some_route?input_file=file.txt&size=30
+    url = route + "?"
+    div = "&"
+    for key, value in attributes.keys():
+        url += f"{key}={str(value)}{div}"
+    return url[:-1]
+
+
+# request.remote_addr
 if __name__ == "__main__":
-    print(get_timeout())
+    att = {"term": 10, "ip": "127.0.0.1"}
+    url = create_url("vote", att)
+    print(url)
