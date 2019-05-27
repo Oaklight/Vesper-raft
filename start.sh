dd5792ada5f386d953f464793aaa6157bca3c5a3
Nicola Manzini <nmanzini@uchicago.edu>
2019-05-27 06:10:49 -0500

    working mess

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
-python3 Vesper/vesper_server.py 0 Vesper/ips.txt
+python3 Vesper/vesper_server.py 0 Vesper/ips.txt &
 python3 Vesper/vesper_server.py 1 Vesper/ips.txt
-# python3 Vesper/vesper_server.py 2 Vesper/ips.txt
-# python3 Vesper/vesper_server.py 3 Vesper/ips.txt
-# python3 Vesper/vesper_server.py 4 Vesper/ips.txt
-
-# http://localhost:5002
-# http://localhost:5003
-# http://localhost:5004
