8ef38ed6f752307f059c203bb3638d6afefa0ed8
Nicola Manzini <nmanzini@uchicago.edu>
2019-05-27 03:28:25 -0500

    debugging flask, requests

--- vesper_server.py
+++ vesper_server.py
@@ -1,5 +1,5 @@
 from node import Node
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import sys
 import logging
 app = Flask(__name__)
@@ -46,12 +46,15 @@ if __name__ == "__main__":
             for ip in f:
                 ip = ip.strip()
                 ip_list.append(ip)
-        print(ip_list)
+        ip_list = [
+            "http://127.0.0.1:5000",
+            "http://127.0.0.1:5001",
+        ]
         my_ip = ip_list.pop(index)
         http, host, port = my_ip.split(':')
         # print(host, port)
         n = Node(ip_list, my_ip)
         logging.info(f"starting {port}")
-        app.run(host="10.150.160.91", port=int(port), debug=True)
+        app.run(host="0.0.0.0", port=int(port), debug=True)
     else:
         print("usage: python server.py index ip_list_file")
